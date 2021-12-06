d4809bbfc1234f59a8cc40a13e72fb200ad3005e
Nicolas Barthès <nicolasbarthes.lana@gmail.com>
2021-12-06 18:13:48 +0100

    First try with cards

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
 // Apparition d'intro (élements)
-const navItems = document.querySelectorAll('.nav-item')
+const navItems = document.querySelectorAll('.nav-item');
 const socials = document.querySelectorAll('.bulle');
+const articles = document.querySelectorAll('.article');
+const section = document.querySelector('.section');
 
 window.addEventListener('load', () => {
     const TL = gsap.timeline({paused : true})
     TL
     .staggerFrom("#logo", 1, {opacity: 0, ease: "power2.out"}, 1, "+=1.6")
     .staggerFrom(navItems, 1, {top: -100, opacity: 0, ease: "power2.out"}, .3)
- 
-    .staggerFrom(socials, 1, {left: -200, ease: "power2.out"}, .3, '+=.1');
-
+    .staggerFrom(socials, .6, {left: -200, ease: "power2.out"}, .3, '-=.1')
+    .staggerFrom(section, 6, {opacity: 0, ease: "power2.out"}, 1, '-=.5');
     TL.play();
   })
 
 // Profile menu
-const profile = document.querySelector('.profile-button')
-const profileMenu = document.getElementById('profile-menu')
+const profile = document.querySelector('.profile-button');
+const profileMenu = document.getElementById('profile-menu');
 
 profile.addEventListener("mouseenter", () => {
   profileMenu.classList.add("active")
-})
+});
 profile.addEventListener("mouseleave", () => {
   profileMenu.classList.remove("active")
-})
-
+});
 profile.addEventListener("click", () => {
   profileMenu.classList.toggle("active")
-})
+});
 
 // Search-data
 const searchIcon = document.querySelector('.search-icon');
-const searchDataWrapper = document.querySelector('.search-data-wrapper')
+const searchDataWrapper = document.querySelector('.search-data-wrapper');
 const closeButton = document.querySelector(".search-data-close-button");
 const searchData = document.querySelector(".search-data");
 
@@ -63,11 +63,10 @@ navToggle.addEventListener("click", () => {
     navToggle.setAttribute('aria-expanded', false);
 
   }
-})
+});
 
 // Transition Vidéo > Background image
 const vid = document.getElementById("heroVideo");
 function videoDisappear() {
    vid.classList.add("videoTransparency")
- }
-
+ };
