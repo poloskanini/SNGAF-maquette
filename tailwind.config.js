9a29d86788acb1296b38ef8ee64b6b507f7a006e
Nicolas Barthès <nicolasbarthes.lana@gmail.com>
2021-12-13 22:13:55 +0100

    (ft) Carousel x5 ok

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      indigo: '#5c6ac4',
+      blue: '#007ace',
+      red: '#de3618',
+      salmon: '#fa8072'
+    },
     extend: {},
   },
   variants: {
